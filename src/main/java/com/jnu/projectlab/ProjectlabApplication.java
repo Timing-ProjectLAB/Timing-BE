e462c969cb9a4e8572fd11dac627858f25e1423c
sangkyounge <shin8877123@naver.com>
2025-04-13 16:59:51 +0900

    기본 환경설정

---
+++ src/main/java/com/jnu/projectlab/ProjectlabApplication.java
@@ -0,0 +1,13 @@
+package com.jnu.projectlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectlabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectlabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jnu/projectlab/ProjectlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnu.projectlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectlabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
