eabf54e45fe64bd77d14812b5212e129c0803b7b
sangkyounge <shin8877123@naver.com>
2025-05-07 21:51:12 +0900

    feat: User Entity Update

--- src/main/java/com/jnu/projectlab/user/User.java
+++ src/main/java/com/jnu/projectlab/user/User.java
@@ -2,17 +2,26 @@ package com.jnu.projectlab.user;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+@Builder
+public class User implements UserDetails { // UserDetails를 상속받아 인증 객체로 사용
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -54,5 +63,40 @@ public class User {
     @PreUpdate
     protected void onUpdate() { //시간을 실시간으로 전달받기 위해 사용
         updatedAt = LocalDateTime.now();
+    }
+
+    // UserDetails 인터페이스 구현 메서드
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userId; // 스프링 시큐리티에서 사용자 식별자로 userId 필드 사용
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // 계정 만료 여부 반환
+        return true; // true -> 만료되지 않음
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // 계정 잠금 여부 반환
+        return true; // true -> 잠금되지 않음
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // 패스워드 만료 여부 반환
+        return true; // true -> 만료되지 않음
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // 계정 사용 가능 여부 변환
+        return true; // true -> 사용 가능
     }
 }
