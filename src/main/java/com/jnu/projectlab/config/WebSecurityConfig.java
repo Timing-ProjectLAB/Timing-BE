e59c37de6dca6e0972a085940b07e886a75e4918
sangkyounge <shin8877123@naver.com>
2025-05-08 00:09:37 +0900

    [#13] Sec feat: urityConfig 추가사항

---
+++ src/main/java/com/jnu/projectlab/config/WebSecurityConfig.java
@@ -0,0 +1,65 @@
+package com.jnu.projectlab.config;
+
+import lombok.RequiredArgsConstructor;
+import com.jnu.projectlab.user.UserDetailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebSecurityConfig {
+
+    private final UserDetailService userService; // 사용자 정보를 가져오는 서비스
+
+    // 스프링 시큐리티 기능 비활성화 (정적 리소스에 대해)
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/static/**"); // static 리소스는 시큐리티 적용 제외
+    }
+
+    // 특정 HTTP 요청에 대한 웹 기반 보안 구성
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/api/user/auth/signup", "/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults()) // HTTP Basic 인증
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login")
+                        .invalidateHttpSession(true)
+                )
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                )
+                .build();
+    }
+
+    // 인증 관리자 관련 설정 (최신 버전 방식)
+    @Bean
+    public AuthenticationManager authenticationManager(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userService); // 사용자 정보 서비스 설정
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder); // 비밀번호 인코더 설정
+
+        return new ProviderManager(authProvider);
+    }
+
+    // 패스워드 인코더로 사용할 빈 등록
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
