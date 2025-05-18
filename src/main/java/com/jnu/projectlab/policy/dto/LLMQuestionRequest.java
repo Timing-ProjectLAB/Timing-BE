4a5109adaebf14ae5ba47850d23bc3ad6debdc95
sangkyounge <shin8877123@naver.com>
2025-05-18 21:58:44 +0900

    feat: llm req dto Add

--- src/main/java/com/jnu/projectlab/policy/dto/LLMQuestionRequest.java
+++ src/main/java/com/jnu/projectlab/policy/dto/LLMQuestionRequest.java
@@ -3,11 +3,13 @@ package com.jnu.projectlab.policy.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class LLMQuestionRequest {
+    @JsonProperty("user_id")
     private String userId;    //ID
     private String question;  // Question
 }
