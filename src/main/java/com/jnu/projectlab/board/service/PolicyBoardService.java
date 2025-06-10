2b67a8bf11549cec9fe3fbde23ea92300a694978
sangkyounge <shin8877123@naver.com>
2025-06-10 15:14:47 +0900

    fix: return값 변경
    빈 배열일경우 상시로 출력

--- src/main/java/com/jnu/projectlab/board/service/PolicyBoardService.java
+++ src/main/java/com/jnu/projectlab/board/service/PolicyBoardService.java
@@ -142,10 +142,10 @@ public class PolicyBoardService {
             
         } catch (Exception e) {
             // 파싱 실패 시
-            return "상시";
+            return "정보없음";
         }
         
-        return "정보없음";
+        return "상시";
     }
 
     /**
